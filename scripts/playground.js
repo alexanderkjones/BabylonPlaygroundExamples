c2287f9d301500d5b5575e46bd511b05e62fbdff
Alex Jones <alexanderkjones@gmail.com>
2022-11-08 16:43:39 -0500

    wip

--- scripts/playground.js
+++ scripts/playground.js
@@ -1,13 +1,11 @@
-const addArcCamera = (scene, canvase, distance) => {
+class TestClass{
+    constructor(scene){
+        this.scene = scene;
+        this.name = test;
+    }
 
-    // This creates and positions an arc rotate camera
-    var camera = new BABYLON.ArcRotateCamera("camera1", -Math.PI / 2, Math.PI / 2 - (Math.PI/10), distance, new BABYLON.Vector3(0, 0, 0));
-    camera.panningSensibility = 100;
-    camera.angularSensibilityX = 800;
-    camera.angularSensibilityY = 800;
-
-    // Attaches camera canvas
-    camera.attachControl(canvas, true);
-
-    return camera
+    logTest(){
+        console.log(this.name);
+    }
+    
 }
