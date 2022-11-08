1a3187851154138f497a06cec9238dbf50960cf7
Alex Jones <alexanderkjones@gmail.com>
2022-11-08 13:23:52 -0500

    testing playground js import

---
+++ scripts/playground.js
@@ -0,0 +1,16 @@
+const addArcCamera = (scene, distance) => {
+
+    // This creates and positions an arc rotate camera
+    var camera = new BABYLON.ArcRotateCamera("camera1", -Math.PI / 2, Math.PI / 2 - (Math.PI/10), distance, new BABYLON.Vector3(0, 0, 0));
+    camera.panningSensibility = 100;
+    camera.angularSensibilityX = 800;
+    camera.angularSensibilityY = 800;
+
+    // Get canvas from scene
+    const canvas = scene.getEngine().getRenderingCanvas();
+
+    // Attaches camera canvas
+    camera.attachControl(canvas, true);
+
+    return camera
+}
